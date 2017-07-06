00e3fa94ee1d633668ee19289c004782dfa2b116
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-07-06 17:15:35 +0900

    production js ready

--- webpack.config.js
+++ webpack.config.js
@@ -24,4 +24,15 @@ var config = {
   }
 }
 
+if(process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
 module.exports = config;

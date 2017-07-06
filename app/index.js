cae9cb634c89002d16d1ddf77b3bec3336dbd12b
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-07-06 16:07:24 +0900

    First install for es6 with babel-loader

---
+++ app/index.js
@@ -0,0 +1,8 @@
+class Banana {
+  print() {
+    console.log("haha");
+  }
+}
+
+var pisang = new Banana;
+pisang.print();
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config;
